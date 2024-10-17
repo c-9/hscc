414666700a51ae141f6445a58b967499b14695c9
chunk <chunk9@163.com>
2024-10-17 09:48:03 +0800

    stage

--- zsim-nvmain/setup.sh
+++ zsim-nvmain/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt-get install -y build-essential g++ scons automake autoconf m4 perl flex bison byacc
+apt-get install -y build-essential g++-4.8 scons automake autoconf m4 perl flex bison byacc python3 python3-pip python3-setuptools
 apt-get install -y libconfig-dev libconfig++-dev libhdf5-dev libelf-dev libxerces-c-dev
-apt-get install -y libboost-all-dev
-# ln -s /usr/include/asm-generic /usr/include/asm
+apt-get install -y libboost-all-dev libgoogle-glog-dev
+pip3 install scons==4.0.0
+ln -s /usr/include/asm-generic /usr/include/asm
