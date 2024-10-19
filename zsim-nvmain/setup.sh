f7002651cf142df39fe2c84bc72bdacd2c482d05
chunk <chunk9@163.com>
2024-10-19 21:02:37 +0800

    stage

--- zsim-nvmain/setup.sh
+++ zsim-nvmain/setup.sh
@@ -4,4 +4,5 @@ apt-get install -y libconfig-dev libconfig++-dev libhdf5-dev libelf-dev libxerce
 apt-get install -y libboost-all-dev libbz2-dev zlib1g-dev libc6-dev libicu-dev
 apt-get install -y python3 python3-pip
 apt-get install -y libgoogle-glog-dev
-# ln -s /usr/include/asm-generic /usr/include/asm
+pip3 install scons==4.0.0
+ln -s /usr/include/asm-generic /usr/include/asm
