2b7295d7ec65415872ab7981e7cf64b56ca7dd88
chunk <chunk9@163.com>
2024-10-21 00:31:11 +0800

    stage

--- zsim-nvmain/compile.sh
+++ zsim-nvmain/compile.sh
@@ -6,30 +6,29 @@ BOOST=$ZSIMPATH/lib/boost_1_59_0
 #LIBCONFIGPATH=$ZSIMPATH/lib/libconfig
 NUMCPUS=$(grep -c ^processor /proc/cpuinfo)
 
-if [ "$1" = "z" ]
+if [ "$1" = "all" ]
 then
-	echo "Compiling only ZSim ..."
-	export PINPATH
-	export NVMAINPATH
-	export BOOST
-	scons -j$NUMCPUS
-else
 	echo "Compiling all ..."
-	
 	#export LIBCONFIGPATH
-	#cd $LIBCONFIGPATH
-	#./configure --prefix=$LIBCONFIGPATH && make install
-	#cd -
-        
-	#cd $ZSIMPATH/contrib/boost_1_59_0
-	#./bootstap./sh
-	#./b2 --clean-all -n
-	#./b2 --buildtype=complete --cxxflags=" -g -std=c++0x -fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0" -j $NUMCPUS
-        #./b2 --buildtype=complete --prefix=$BOOST install
-	#cd -
+        #cd $LIBCONFIGPATH
+        #./configure --prefix=$LIBCONFIGPATH && make install
+        #cd -
 
-	export PINPATH
-	export NVMAINPATH
-	export BOOST
-	scons -j$NUMCPUS
+        cd $ZSIMPATH/contrib/boost_1_59_0
+        ./bootstrap./sh
+        ./b2 --toolset=gcc-4.8 --clean-all -n
+        ./b2 --toolset=gcc-4.8 --buildtype=complete --cxxflags=" -g -std=c++0x -fabi-version=2 -D_GLIBCXX_USE_CXX11_ABI=0" -j $NUMCPUS
+        ./b2 --toolset=gcc-4.8 --buildtype=complete --prefix=$BOOST install
+        cd -
+
+        export PINPATH
+        export NVMAINPATH
+        export BOOST
+        scons -j$NUMCPUS
+else
+	echo "Compiling only ZSim ..."
+        export PINPATH
+        export NVMAINPATH
+        export BOOST
+        scons -j$NUMCPUS
 fi
